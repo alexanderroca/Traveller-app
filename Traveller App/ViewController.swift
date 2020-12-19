0c14d14fd39d9b31364cd1a393a230a9b50b5b95
JeezLuis <luisfarrecastan@gmail.com>
2020-12-19 22:29:56 +0100

    Initial Commit

---
+++ Traveller App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traveller App
+//
+//  Created by Luis Farré Castán on 19/12/20.
+//  Copyright © 2020 Alexander Roca - Joan Esteban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
