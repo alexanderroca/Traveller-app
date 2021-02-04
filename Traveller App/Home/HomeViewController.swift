0fa9e2348e079013598d68f862be8bbaad6383c6
janesteban <32568975+janesteban@users.noreply.github.com>
2021-02-04 04:56:38 +0100

    Home

--- Traveller App/Home/HomeViewController.swift
+++ Traveller App/Home/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate{
 
@@ -42,6 +43,8 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
         var name: String
         var eventImage: UIImage
         var location: String
+        var category: String
+        var eventID: String
     }
     
     var _experiences: [Event] = []
@@ -58,6 +61,8 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getExperiences()
+        //activitiesCollectionView.reloadData()
         
     }
 
@@ -70,17 +75,17 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     
         switch activityType.selectedSegmentIndex {
-            case 0: return experiences.count
+            case 0: return _experiences.count
                     //TODO: to be debugged
-                    //_experiences.count
+                    
                     //ENDs here
-            case 1: return adventures.count
+            case 1: return _adventures.count
                     //TODO: to be debugged
-                    //_adventures.count
+                    //
                     //ENDs here
-            case 2: return activities.count
+            case 2: return _activities.count
                     //TODO: to be debugged
-                    //_activities.count
+                    //
                     //ENDs here
             default: return 0
         }   //switch
@@ -93,36 +98,43 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
         
         switch activityType.selectedSegmentIndex {
             case 0:
+                /*
                 cell.activity.text = experiences[indexPath.item]
                 cell.activityImage.image = activityImages0[indexPath.item]
                 cell.location.text = locations0[indexPath.item]
+                */
                 
-                //TODO: to be debugged
-                //cell.activity.text = _experiences[indexPath.item].name
-                //cell.activityImage.image = _experiences[indexPath.item].eventImage
-                //cell.location.text = _experiences[indexPath.item].location
+                cell.activity.text = _experiences[indexPath.item].name
+                cell.activityImage.image = _experiences[indexPath.item].eventImage
+                cell.location.text = _experiences[indexPath.item].location
                 //ENDs here
                 break
             case 1:
+                
+                /*
                 cell.activity.text = adventures[indexPath.item]
                 cell.activityImage.image = activityImages1[indexPath.item]
                 cell.location.text = locations1[indexPath.item]
+                */
                 
                 //TODO: to be debugged
-                //cell.activity.text = _adventures[indexPath.item].name
-                //cell.activityImage.image = _adventures[indexPath.item].eventImage
-                //cell.location.text = _adventures[indexPath.item].location
+                cell.activity.text = _adventures[indexPath.item].name
+                cell.activityImage.image = _adventures[indexPath.item].eventImage
+                cell.location.text = _adventures[indexPath.item].location
                 //ENDs here
                 break
             case 2:
+                
+                /*
                 cell.activity.text = activities[indexPath.item]
                 cell.activityImage.image = activityImages2[indexPath.item]
                 cell.location.text = locations2[indexPath.item]
+                */
                 
                 //TODO: to be debugged
-                //cell.activity.text = _activities[indexPath.item].name
-                //cell.activityImage.image = _activities[indexPath.item].eventImage
-                //cell.location.text = _activities[indexPath.item].location
+                cell.activity.text = _activities[indexPath.item].name
+                cell.activityImage.image = _activities[indexPath.item].eventImage
+                cell.location.text = _activities[indexPath.item].location
                 //ENDs here
                 break
             default: break
@@ -137,35 +149,44 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
             
             switch activityType.selectedSegmentIndex {
                 case 0:
+                    
+                    /*
                     vc?.name = experiences[indexPath.row]
                     vc?.image = activityImages0[indexPath.row]
                     vc?.location = locations0[indexPath.row]
-                    
+                    */
+ 
                     //TODO: to be debugged
-                    //vc?.name = _experiences[indexPath.row].name
-                    //vc?.image = _experiences[indexPath.row].eventImage
-                    //vc?.location = _experiences[indexPath.row].location
+                    vc?.name = _experiences[indexPath.row].name
+                    vc?.image = _experiences[indexPath.row].eventImage
+                    vc?.location = _experiences[indexPath.row].location
                     //ENDs here
                     break
                 case 1:
+                    
+                    /*
                     vc?.name = adventures[indexPath.row]
                     vc?.image = activityImages1[indexPath.row]
                     vc?.location = locations1[indexPath.row]
-                
+                    */
+ 
                     //TODO: to be debugged
-                    //vc?.name = _adventures[indexPath.row].name
-                    //vc?.image = _adventures[indexPath.row].eventImage
-                    //vc?.location = _adventures[indexPath.row].location
+                    vc?.name = _adventures[indexPath.row].name
+                    vc?.image = _adventures[indexPath.row].eventImage
+                    vc?.location = _adventures[indexPath.row].location
                     //ENDs here
                 case 2:
+                    
+                    /*
                     vc?.name = activities[indexPath.row]
                     vc?.image = activityImages2[indexPath.row]
                     vc?.location = locations2[indexPath.row]
-                
+                    */
+                    
                     //TODO: to be debugged
-                    //vc?.name = _activities[indexPath.row].name
-                    //vc?.image = _activities[indexPath.row].eventImage
-                    //vc?.location = _activities[indexPath.row].location
+                    vc?.name = _activities[indexPath.row].name
+                    vc?.image = _activities[indexPath.row].eventImage
+                    vc?.location = _activities[indexPath.row].location
                     //ENDs here
                 default: break
             }   //switch
@@ -173,4 +194,101 @@ class HomeViewController: UIViewController , UICollectionViewDataSource, UIColle
             self.navigationController?.pushViewController(vc!, animated: true)
         }
     }
+    
+    
+    
+    //All of them
+    func getExperiences(){
+        
+        let db = Firestore.firestore()
+        
+        db.collection("experiences")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    
+                    for document in querySnapshot!.documents {
+                        
+                        print("\(document.documentID) => \(document.data())")
+                        
+                        let data = document.data()
+                        
+                        let name = data["name"] as! String
+                        let location = data["location"] as! String
+                        let category = data["category"] as! String
+                        let imageString = data["image"] as! String
+                        
+                        let eventID = document.documentID
+                        
+                        
+                        //generate image
+                        var image2 = UIImage(named: "default-experience")
+                        
+                        
+                         do{
+                            
+                            let imageUrl = URL(string: imageString)
+                            
+                            if imageUrl != nil {
+                                let imageData = try Data(contentsOf: imageUrl!)
+                                image2 = UIImage(data: imageData)
+                            }else{
+                                image2 = UIImage(named: "default-experience")
+                            }
+                            
+                             
+                         }catch{
+                             //Use default image
+                            image2 = UIImage(named: "default-experience")
+                            
+                         }
+                        
+                        let event = Event(name: name, eventImage: image2!, location: location, category: category, eventID: eventID)
+                        
+                        
+                        switch category {
+                        
+                            case "activity":
+                                //add to activity array
+                                self._activities.append(event)
+                                //Add to experiences array
+                                self._experiences.append(event)
+                                
+                                break
+                                
+                            case "adventure":
+                                //add to adventures array
+                                self._adventures.append(event)
+                                //Add to experiences array
+                                self._experiences.append(event)
+                                break
+                            
+                            default:
+                                print("ERROR: UNDEFINED category")
+                            }
+                        
+                    }
+                    
+                }
+                
+                
+        }
+        
+        
+    }
+    
+    //Only activities
+    func getActivities(){
+        
+        
+    }
+    
+    //Only adventures
+    func getAdventures(){
+        
+        
+    }
+    
+    
 }
